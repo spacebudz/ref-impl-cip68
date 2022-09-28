deae4250a266d51b9543d73eda7f66a21d3b4900
alessandrokonrad <akonrad6@gmail.com>
2022-09-28 11:49:57 +0200

    first commit

---
+++ 222.ts
@@ -0,0 +1,4 @@
+// Mint an NFT with the 222 token standard within CIP-0068
+
+import { Blockfrost, Lucid } from "https://deno.land/x/lucid@0.6.5/mod.ts";
+import * as helios from "https://raw.githubusercontent.com/Hyperion-BT/Helios/v0.6.5/helios.js";
---
+++ README.md
@@ -0,0 +1,1 @@
+# ref-impl-cip68
